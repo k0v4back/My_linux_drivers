2a5142b72c211d397137a3e6e939946808b402a1
k0v4 <vadkos33@outlook.com>
2022-05-08 16:03:02 +0300

    Edit Makefile and init ssd1306

--- application/ssd1306.h
+++ application/ssd1306.h
@@ -1,7 +1,15 @@
 #ifndef _SSD1306_H
 #define _SSD1306_H
 
+#define SSD1306_DRIVER_NAME     "ssd1306"
+#define I2C_ADAPTER             "/dev/i2c-2"
 
+enum SSD1306_CONSTS{
+    ERROR_SSD1306_INIT,
+    SUCCESS_SSD1306_INIT
+};
+
+int check_init_ssd1306(void);
 
 #endif  // _SSD1306_H
 
